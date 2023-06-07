43fa75a5f05f04519a2a383d8d11e1918804d442
Valentin Simenov <simeonov2304@gmail.com>
2023-06-07 03:08:15 +0300

    Update

--- teammemberallocations/src/Employees.jsx
+++ teammemberallocations/src/Employees.jsx
@@ -1,6 +1,4 @@
-import {useState} from 'react';
-import femaleProfile from './images/femaleProfile.jpg'
-import maleProfile from './images/maleProfile.jpg'
+
 import Teams from './Teams';
 import TeamMembers from "./TeamMembers";
 
