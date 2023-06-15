55745c3286b35ebf0261c04c80abadc97a868b96
Valentin Simenov <simeonov2304@gmail.com>
2023-06-15 17:43:40 +0300

    Update

--- meals_application/src/context.jsx
+++ meals_application/src/context.jsx
@@ -1,23 +1,26 @@
-import React, {useContext, useEffect} from 'react'
-
+import React, {useContext, useEffect, useState} from 'react'
+import axios from 'axios'
 const AppContext = React.createContext()
+
+
 
 const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
 const randomMeal = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
 
 const AppProvider = ({children}) => {
 
-    useEffect(()=>{
-        const fetchData = async () =>{
-            try{
-                const response = await fetch('https://randomuser.me/api/')
-                const data = await response.json()
-                console.log(data)
-            }catch(error){
-                console.log(error)
-            }
+    const fetchMeals = async (url) =>{
+        try{
+            const response = await axios(url)
+
+            console.log(response.data)
+        }catch(error){
+            console.log(error.response)
         }
-        fetchData()
+    }
+
+    useEffect(()=>{
+        fetchMeals(allMealsUrl)
     }, [])
 
     return <AppContext.Provider value={
