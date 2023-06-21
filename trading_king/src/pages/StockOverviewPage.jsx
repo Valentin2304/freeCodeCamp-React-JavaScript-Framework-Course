e4bef744bb301ef109caa48760f4607451c2d813
Valentin Simenov <simeonov2304@gmail.com>
2023-06-21 21:43:22 +0300

    Update

--- trading_king/src/pages/StockOverviewPage.jsx
+++ trading_king/src/pages/StockOverviewPage.jsx
@@ -1,7 +1,7 @@
 import {AutoComplete} from "../components/AutoComplete";
 import {StockList} from "../components/StockList";
 export const StockOverviewPage = () => {
-    return <div className="text-center fw-bold fs-5">StockOverviewPage
+    return <div className="text-center fw-bold fs-5">
         <AutoComplete/>
         <StockList/>
         </div>
