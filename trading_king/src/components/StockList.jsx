2bf26701f62e7bf6861a74b75a1c02a0cd3cd761
Valentin Simenov <simeonov2304@gmail.com>
2023-06-21 21:16:02 +0300

    Update

--- trading_king/src/components/StockList.jsx
+++ trading_king/src/components/StockList.jsx
@@ -7,7 +7,7 @@ import {useNavigate} from 'react-router-dom'
 
 export const StockList = () => {
     const [stock, setStock] = useState([])
-    const {watchList} = useContext(WatchListContext)
+    const {watchList, deleteStock} = useContext(WatchListContext)
     const navigate = useNavigate()
 
 
@@ -69,7 +69,9 @@ export const StockList = () => {
                              <td>{stockData.data.h}</td>
                              <td>{stockData.data.l}</td>
                              <td>{stockData.data.o}</td>
-                             <td>{stockData.data.pc}</td>
+                             <td>{stockData.data.pc} <button onClick={(e)=>{
+                                 e.stopPropagation()
+                                 deleteStock(stockData.symbol)}}className='btn-danger btn-sm ml-3 d-inline-block delete-button'>Remove</button></td>
                          </tr>
                      )
                  })}
